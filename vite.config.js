a4fba2aebd4815b024e16045ab562d5217187369
Brian Twene <twene521@gmail.com>
2023-02-17 22:36:23 +0000

    inital commit

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from "vite-plugin-pwa"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    https: true
+  },
+  plugins: [react(), VitePWA({
+    manifest: {
+      name: 'ColorSiphon',
+      short_name: 'Siphon',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#ffffff',
+      lang: 'en',
+      scope: '/',
+      icons: [
+        {
+          src: '/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+        {
+          src: '/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+      theme_color: '#ffffff',
+    },
+  })],
+})
